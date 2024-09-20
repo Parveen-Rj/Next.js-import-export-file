641bac6151e4586f85ac3258a896866e4a973d26
Parveen-Rj <165608618+Parveen-Rj@users.noreply.github.com>
2024-09-20 09:49:33 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,13 @@
+
+import Header from "./components/Header";
+import About from "./components/About";
+ function Home() {
+  return (
+    <div>
+    <h1>this is home page home page</h1>
+    <Header />
+      <About />
+    </div>
+  );
+}
+export default Home;
